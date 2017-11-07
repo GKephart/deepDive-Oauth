fcd9d51c9e0313e5cacd7848defd50558e020e2b
George Kephart <Gkephart@cnm.edu>
2017-11-07 13:38:39 -0700

    created the constructor method

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -13,7 +13,6 @@ require_once("autoload.php");
  *
  * @author George Kephart <gkephart@cnm.edu>
  */
-
 class Profile implements \JsonSerializable {
 	use ValidateUuid;
 
@@ -40,6 +39,31 @@ class Profile implements \JsonSerializable {
 	 * string $profileUsername
 	 **/
 	private $profileUsername;
+
+	/**
+	 * @param string | Uuid $newProfileId primary key for the Profile Object
+	 * @param string $newProfileImage path to the users avatar stored on github\
+	 * @param string $newProfileOauthToken token for Oauth handshake
+	 * @param string $newProfileUsername name of the user
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if a data type violates a data hint
+	 * @throws \Exception if some other exception occurs
+	 * @see https://php.net/manual/en/language.oop5.decon.php
+	 */
+	public function __construct($newProfileId, string $newProfileImage, string $newProfileOauthToken, string $newProfileUsername) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileImage($newProfileImage);
+			$this->setProfileOauthToken($newProfileOauthToken);
+			$this->setProfileImage($newProfileUsername);
+
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			//determine what exception type was thrown
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
 
 
 	public function jsonSerialize() {
